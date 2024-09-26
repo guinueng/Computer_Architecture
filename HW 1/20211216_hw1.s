ad8ae6a2711d39e354cdaa5df38d4a70591dffc6
guinueng <guinueng@unist.ac.kr>
2024-09-26 19:34:59 +0900

    Update 20211216_hw1.s
    Added some notation.

--- HW 1/20211216_hw1.s
+++ HW 1/20211216_hw1.s
@@ -62,13 +62,15 @@ fibonacci:
     beq $t0, $zero, L1 # If $t0 != 0 then move line L1.
     # Case 1. n == 0 case.
     li $v0, 0 # Make return value 0.
-    j L3 # Jump to line L3 to pursue restoring 
+    j L3 # Jump to line L3 to pursue restoring.
+
 L1: 
     slti $t0, $a0, 2 # If $a0 < 2, store 1 at $t0. Else, store 0.
     beq $t0, $zero, L2 # If $t0 != 0 then move line L1.
     # Case 2. n == 1 case.
     li $v0, 1 # Make return value 1.
-    j L3
+    j L3 # Jump to line L3 to pursue restoring.
+
 L2: # Case 3. n > 1 case.
     addi $a0, $a0, -1 # n = n - 1.
     jal fibonacci # Do recursive fibonacci(n-1).
@@ -79,7 +81,8 @@ L2: # Case 3. n > 1 case.
     
     move $s2, $v0 # Store result to register $s2.
     add $v0, $s1, $s2 # return value = fibonacci (n - 1) + fibonacci(n - 2)
-L3:
+L3: # Last part of fibonacci function.
+    # In first implementation, fount that there are duplicated, thus integrated into one area.
     lw $ra, 8($sp) # Restore return address to $ra.
     lw $a0, 4($sp) # Restore assigned value to $a0.
     lw $s1, 0($sp) # Restore register $s1 value to $s1.
